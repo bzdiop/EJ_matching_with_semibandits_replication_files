83ba117f881690599430a79d880ab4fe61d9b69f
bzdiop <62705278+bzdiop@users.noreply.github.com>
2022-04-16 00:50:53 +0100

    QA test

--- 00_master.R
+++ 00_master.R
@@ -1,9 +1,17 @@
 # 4/14/2022 by bzdiop
 
-#Packages used: uncomment this line if needed. 
-# install.packages(c("tidyverse", "lpSolve", "lubridate", "patchwork", "furrr", "patchwork"))
-install.packages("rstan", repos = c("https://mc-stan.org/r-packages/", getOption("repos")))
+
+## Packages used: uncomment this line if needed. 
+# install.packages(c("tidyverse", "lpSolve", "lubridate", "patchwork", "furrr", "patchwork", "rstan", "StanHeaders"))
+
+## If you get any "Build Tools" errors, make sure you have a recent enough version of R/Rstudio
+
+rm(list=ls())
 
 source("dataprep.R") #this script also calls "adaptive_matching_functions.R" which in turns calls additive_effects_logit_aggregated.stan
-source("simulations.R") #this script also calls "adaptive_matching_functions.R" which in turns calls additive_effects_logit_aggregated.stan
+source("simulation.R") #this script also calls "adaptive_matching_functions.R" which in turns calls additive_effects_logit_aggregated.stan
 source("summarize_simulations.R")
+
+
+
+#started at 3:17pm -- ended: 11:15pm
